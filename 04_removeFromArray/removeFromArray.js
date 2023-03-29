eb73a0603974f64e77bf9203861bec7fceb06572
maashh <58293726+maashhCS@users.noreply.github.com>
2023-03-29 02:12:53 +0200

    finished 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...items) {
+	for (const item of items) {
+		let indexOfItem = array.findIndex((e) => e === item);
+		if (indexOfItem > -1) {
+			array.splice(indexOfItem, 1);
+		}
+	}
+	return array;
 };
 
 // Do not edit below this line
