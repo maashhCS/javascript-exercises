ded2bf3a6dde6ac52f6c9fa19490d6452a19e16d
maashh <58293726+maashhCS@users.noreply.github.com>
2023-04-01 16:57:36 +0200

    finished 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(num) {
+  let sum = 0;
+  for(let i = 0; i < num.length; i++){
+    sum += num[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(num) {
+  let multi = 1;
+  for(let i = 0; i < num.length; i++){
+    multi = multi * num[i]; 
+  }
+  return multi;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0) return 1;
+  let product = 1;
+  for (let i = num; i > 0; i--) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
