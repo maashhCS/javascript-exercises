f7fd2c18173ecc9cdd59618e1392949025341527
maashh <58293726+maashhCS@users.noreply.github.com>
2023-04-01 18:11:02 +0200

    finished 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(typeof(num) === "string"){
+        num = Number(num);
+    } else if (num < 0){
+        return "OOPS";
+    };
+    let lastNum = 0;
+    let currNum = 1;
+    let returnNum = 1;
+    for(let i = 1; i < num; i++){
+        returnNum = lastNum + currNum;
+        lastNum = currNum;
+        currNum = returnNum;
+    }
+    return returnNum;
 };
 
 // Do not edit below this line
